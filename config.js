864c17f06e52b3439c6395ceccad64c774aed52d
ranzzbot <117565862+ranzzbot@users.noreply.github.com>
2022-11-10 13:00:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['6285390216377']
+global.premium = ['6285390216377']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
